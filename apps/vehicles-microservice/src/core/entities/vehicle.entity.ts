bfd24f8834dbbfd8c022e4b5b7de3e2cdcd3e286
gustavobarsan <gustavobarsan@gmail.com>
2025-09-06 16:09:22 -0300

    chore: :construction: vehicle entity

---
+++ apps/vehicles-microservice/src/core/entities/vehicle.entity.ts
@@ -0,0 +1,18 @@
+export class Vehicle {
+  id: string;
+  plate: string;
+  chassis: string;
+  renavam: string;
+  model: string;
+  brand: string;
+  year: number;
+  color: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(props: Partial<Vehicle>) {
+    Object.assign(this, props);
+    if (!this.createdAt) this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+}
