8be06fdec261225658075c5c9bae17cbce38badf
gustavobarsan <gustavobarsan@gmail.com>
2025-09-07 15:54:51 -0300

    chore: :construction: api gateway controller

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -1,12 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiGatewayService } from './api-gateway.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { firstValueFrom } from 'rxjs';
 
-@Controller()
+@Controller('vehicles')
+@UsePipes(new ValidationPipe({ whitelist: true }))
 export class ApiGatewayController {
-  constructor(private readonly apiGatewayService: ApiGatewayService) {}
+  constructor(@Inject('VEHICLES_SERVICE') private client: ClientProxy) {}
+
+  @Post()
+  create(@Body() createVehicleDto: CreateVehicleDto) {
+    return firstValueFrom(
+      this.client.send('create_vehicle', createVehicleDto),
+    );
+  }
 
   @Get()
-  getHello(): string {
-    return this.apiGatewayService.getHello();
+  findAll() {
+    return firstValueFrom(this.client.send('find_all_vehicles', {}));
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return firstValueFrom(this.client.send('find_one_vehicle', id));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
+    return firstValueFrom(
+      this.client.send('update_vehicle', { id, updateVehicleDto }),
+    );
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return firstValueFrom(this.client.send('remove_vehicle', id));
   }
 }
