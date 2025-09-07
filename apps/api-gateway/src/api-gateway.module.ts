5601e453cb3a7aca3becb303a8a88b3dfd277534
gustavobarsan <gustavobarsan@gmail.com>
2025-09-07 15:53:57 -0300

    chore: :construction: add module transporter conection

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'VEHICLES_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://rabbitmq:5672'],
+          queue: 'vehicles_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),    
+  ],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
 })
