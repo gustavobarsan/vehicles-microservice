43663f2fcbe7eda1e5708217ccaafa39547fab7e
gustavobarsan <gustavobarsan@gmail.com>
2025-09-07 12:29:54 -0300

    fix: :fire: Veihcles Module DI

--- apps/vehicles-microservice/src/nest-adapters/vehicles/vehicles.module.ts
+++ apps/vehicles-microservice/src/nest-adapters/vehicles/vehicles.module.ts
@@ -4,17 +4,22 @@ import { VehiclesController } from './vehicles.controller';
 import { PrismaService } from '../../infra/database/prisma/prisma.service';
 import { VehiclesRepository } from '../../core/repositories/vehicles.repository';
 import { PrismaVehiclesRepository } from '../../infra/database/prisma/repositories/prisma.vehicles.repository';
-
+import { VehiclesUseCases } from '../../core/use-cases/vehicles.use-cases';
 
 @Module({
   controllers: [VehiclesController],
   providers: [
-    VehiclesService,
-    PrismaService,
     {
       provide: VehiclesRepository,
       useClass: PrismaVehiclesRepository,
     },
+    {
+      provide: VehiclesUseCases,
+      useFactory: (repository: VehiclesRepository) => new VehiclesUseCases(repository),
+      inject: [VehiclesRepository],
+    },
+    VehiclesService,
+    PrismaService,
   ],
 })
 export class VehiclesModule {}
